f5f0284d32716f37775339d280b4e2e156ae51f4
KirstenDunst <cao_shixin@yahoo.com>
2017-07-20 11:52:55 +0800

    基类实现相机的几个特效

---
+++ PhotoByAVFou/PhotoByAVFou/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoByAVFou
+//
+//  Created by CSX on 2017/7/20.
+//  Copyright © 2017年 宗盛商业. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
